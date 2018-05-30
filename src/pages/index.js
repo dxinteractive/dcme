9cda6d59b0c8ed55b69b08cbff4cc044a84d8025
Damien Clarke <dxinteractive@gmail.com>
2018-05-30 20:24:25 +1000

    More

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 // @flow
 import React from 'react';
 import type {Node} from 'react';
-//import Link from 'gatsby-link'; 
+import {Box, Landing, Text} from '../style';
+//import Link from 'gatsby-link';
 //    <Link to="/page-2/">Go to page 2</Link>
 
 export default (): Node => {
-    return <div>
-        <h1>damienclarke.me</h1>
-        <p>Welcome to your new Gatsby site.</p>
-    </div>;
+    return <Landing>
+        <Box>
+            <Text element="h1" modifier="sizeGiga">damienclarke.me</Text>
+            <Text element="p" modifier="margin">Welcome to your new Gatsby site.</Text>
+        </Box>
+    </Landing>;
 };
