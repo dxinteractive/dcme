7202b1f01e41b0e99e3f9db89cf00aa9cf5929cd
Damien Clarke <dxinteractive@gmail.com>
2019-06-13 18:11:17 +1000

    feat: chang mosaic text

--- src/pages/code/mosaic-maker.js
+++ src/pages/code/mosaic-maker.js
@@ -1,18 +1,16 @@
 // @flow
 import React from 'react';
 import {Box} from 'dcme-style';
+import {Link} from 'dcme-style';
 import {Text} from 'dcme-style';
 
 import DeadEndPage from '../../component/DeadEndPage';
 
 export default () => <DeadEndPage
-    title={() => <Text element="h1" modifier="sizeTera center">It's gone</Text>}
+    title={() => <Text element="h1" modifier="sizeTera center">Mosaic Maker</Text>}
     content={() => <Box>
         <Text element="p" modifier="monospace center margin">
-            Mosaic maker is no longer available, as Adobe Flash is becoming increasingly unavailable. I have no plans to remake this.
-        </Text>
-        <Text element="p" modifier="monospace center margin">
-            Thanks for using it. It was fun while it lasted.
+            As Adobe Flash Player is becoming increasingly unavailable in all major browsers, you may become unable to use mosaic maker. <Link href="/assets/mosaic.html">Follow this link and cross your fingers</Link>.
         </Text>
     </Box>}
 />;
